34cdd38449f28b8404c23c265a3ddb193801a72d
Jay Shah <jayshah5696@gmail.com>
2026-04-08 17:56:35 -0700

    Fix Intel SHA for Medha 0.3.1 cask

--- Casks/medha.rb
+++ Casks/medha.rb
@@ -3,7 +3,7 @@ cask "medha" do
 
   version "0.3.1"
   sha256 arm:   "e41fcc67e097b5890a98f22ee70da7e233362d1f27c93cfc30d7e15ee8f0604d",
-         intel: "754abd4d5e5b956a1336e56c77cd29d70d96a864d0e7a6f67916df261e584e13"
+         intel: "1d1cdc01f9a062e78b4e432b431f4331c43fa2b7869aae321d2329fc77da6370"
 
   url "https://github.com/jayshah5696/medha/releases/download/v#{version}/Medha-#{version}-#{arch}.dmg"
   name "Medha"
